34edd5d0a884879f703fc62594bacc42e5b2da4b
Jens <jr@jensravn.com>
2021-06-25 15:11:09 +0200

    connect website with api

--- movielist-website/next.config.js
+++ movielist-website/next.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   reactStrictMode: true,
-}
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://gcp-playground-jens.ew.r.appspot.com/:path*",
+      },
+    ];
+  },
+};
