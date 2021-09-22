61ce88308a70b52c23ed1f71624450db34515abb
Jens Ravn Jensen <jensravn102@gmail.com>
2021-09-22 20:56:49 +0200

    Springapi (#7)
    * Serve images from bucket
    
    * update diagram
    
    * springapi
    
    * status check

---
+++ springapi/src/main/java/AppConfig.java
@@ -0,0 +1,6 @@
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan({"com.jensravn.springapi"})
+public class AppConfig {}
---
+++ springapi/src/main/java/com/jensravn/springapi/models/Movie.java
@@ -0,0 +1,40 @@
+package com.jensravn.springapi.models;
+
+import java.util.List;
+
+public class Movie {
+
+  private final int id;
+  private final String title;
+  private final String description;
+  private final String image;
+  private final List<String> genre;
+
+  public Movie(int id, String title, String description, String image, List<String> genre) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.image = image;
+    this.genre = genre;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public String getImage() {
+    return image;
+  }
+
+  public List<String> getGenre() {
+    return genre;
+  }
+}
