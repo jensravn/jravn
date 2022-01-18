ffdf45e9400ef6491481a8d30139357cc936cde8
Jens <jr@jensravn.com>
2022-01-18 01:19:43 +0100

    websocket

--- go-webservice/main.go
+++ go-webservice/main.go
@@ -1,26 +1,66 @@
 package main
 
 import (
-	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"time"
+
+	"golang.org/x/net/websocket"
 )
 
-func main() {
-	handler := http.HandlerFunc(handleRequest)
-	http.Handle("/", handler)
-	http.ListenAndServe(":8080", nil)
+type message struct {
+	Data string `json:"data"`
+	Type string `json:"type"`
 }
 
-func handleRequest(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusCreated)
-	w.Header().Set("Content-Type", "application/json")
-	resp := make(map[string]string)
-	resp["message"] = "Hello World"
-	jsonResp, err := json.Marshal(resp)
-	if err != nil {
-		log.Fatalf("Error happened in JSON marshal. Err: %s", err)
+func MessageWebSocket(ws *websocket.Conn) {
+	// we can verify that the origin is an allowed origin
+	fmt.Printf("origin: %s\n", ws.Config().Origin)
+
+	done := make(chan struct{})
+	go func(c *websocket.Conn) {
+		for {
+			var msg message
+			if err := websocket.JSON.Receive(ws, &msg); err != nil {
+				log.Println(err)
+				break
+			}
+			fmt.Printf("recieved message %s\n", msg.Data)
+		}
+		close(done)
+	}(ws)
+
+loop:
+	for {
+		select {
+		case <-done:
+			fmt.Println("connection was closed, lets break out of here")
+			break loop
+		default:
+			fmt.Println("sending message")
+			message := []message{{
+				Data: "datadata",
+				Type: "typetype",
+			}}
+			if err := websocket.JSON.Send(ws, message); err != nil {
+				log.Println(err)
+				break
+			}
+			// pause for 3 seconds before sending again
+			time.Sleep(3 * time.Second)
+		}
 	}
-	w.Write(jsonResp)
-	return
+	fmt.Println("closing the connection")
+	defer ws.Close()
+}
+
+func main() {
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fs)
+	http.Handle("/ws", websocket.Handler(MessageWebSocket))
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
