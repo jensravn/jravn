1d832ca82fbc9e393f64c8aea3526624cf69da20
Jens Ravn Jensen <jensravn102@gmail.com>
2022-01-31 23:02:20 +0100

    terraform init (#71)
    * terraform init
    
    * terraform cloudbuild
    
    * dir: "terraform"
    
    * .
    
    * .2
    
    * .3
    
    * validate
    
    * init then validate
    
    * remove credentials
    
    * tfstate
    
    * remove credentials from provider
    
    * fmt

---
+++ terraform/main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.6.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "gcp-playground-jens"
+  region  = "europe-west1"
+  zone    = "europe-west1-b"
+}
+
+resource "google_cloud_run_service" "run_go_webservice" {
+  name     = "go-webservice"
+  location = "europe-west1"
+
+  template {
+    spec {
+      containers {
+        image = "eu.gcr.io/gcp-playground-jens/go-webservice"
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
+
+terraform {
+  backend "gcs" {
+    bucket = "gcp-playground-jens-terraform"
+    prefix = "state"
+  }
+}
