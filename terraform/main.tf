fd1e3242b89dac7c509e6742d4afe5f6c6e3f980
jerav1 <jens.ravn.jensen@ingka.com>
2022-01-31 23:24:57 +0100

    terraform - publicly accessable

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,6 +13,23 @@ provider "google" {
   zone    = "europe-west1-b"
 }
 
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location = google_cloud_run_service.default.location
+  project  = google_cloud_run_service.default.project
+  service  = google_cloud_run_service.default.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
+
 resource "google_cloud_run_service" "run_go_webservice" {
   name     = "go-webservice"
   location = "europe-west1"
