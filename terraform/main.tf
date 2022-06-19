5a1a00c077f14d21a9c5d54bf14324009f2a268e
Jens Ravn Jensen <jensravn102@pm.me>
2022-06-19 02:17:46 +0200

    Add go-cmd-terraform to Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,6 +13,24 @@ provider "google" {
   zone    = "europe-west1-b"
 }
 
+resource "google_cloud_run_service" "go_cmd_pubsub" {
+  name     = "go-cmd-pubsub"
+  location = "europe-west1"
+
+  template {
+    spec {
+      containers {
+        image = "eu.gcr.io/gcp-playground-jens/go-cmd-pubsub"
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
+
 data "google_iam_policy" "noauth" {
   binding {
     role = "roles/run.invoker"
@@ -23,14 +41,14 @@ data "google_iam_policy" "noauth" {
 }
 
 resource "google_cloud_run_service_iam_policy" "noauth" {
-  location = google_cloud_run_service.run_web_service.location
-  project  = google_cloud_run_service.run_web_service.project
-  service  = google_cloud_run_service.run_web_service.name
+  location = google_cloud_run_service.go_cmd_web.location
+  project  = google_cloud_run_service.go_cmd_web.project
+  service  = google_cloud_run_service.go_cmd_web.name
 
   policy_data = data.google_iam_policy.noauth.policy_data
 }
 
-resource "google_cloud_run_service" "run_web_service" {
+resource "google_cloud_run_service" "go_cmd_web" {
   name     = "go-cmd-web"
   location = "europe-west1"
 
