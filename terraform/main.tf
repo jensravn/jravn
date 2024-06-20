acf39d5dcce66190a2fe520b8a554928cdd00795
Jens Ravn Jensen <3356452+jensravn@users.noreply.github.com>
2024-06-20 14:25:01 +0200

    feat: create artifact registry (#6)

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,3 +24,25 @@ provider "google" {
   region  = local.region
   zone    = local.zone
 }
+
+resource "google_artifact_registry_repository" "cloud_run" {
+  format                 = "DOCKER"
+  location               = local.region
+  repository_id          = "cloud-run"
+  cleanup_policy_dry_run = false
+  // Cleanup policies. Keep policies will take precedence over delete policies.
+  cleanup_policies {
+    id     = "delete-if-older-than-30-days"
+    action = "DELETE"
+    condition {
+      older_than = "2592000s" // 30 days
+    }
+  }
+  cleanup_policies {
+    id     = "keep-minimum-versions"
+    action = "KEEP"
+    most_recent_versions {
+      keep_count = 5
+    }
+  }
+}
