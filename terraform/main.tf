b597eb84c072da46564235bff807aecef8709bfd
Jens Ravn Jensen <3356452+jensravn@users.noreply.github.com>
2024-06-20 22:54:38 +0200

    ci: build and push (#8)
    * ci: build and push
    
    * login
    
    * ./go
    
    * label
    
    * \
    
    * - working-directory: terraform
    
    * terraform job
    
    * needs: [build-and-push]
    
    * GOOGLE_CREDENTIALS
    
    * google_cloud_run_v2_service
    
    * no plan
    
    * Allow unauthenticated invocations
    
    * checks + deploy
    
    * latest
    
    * \
    
    * git rev-parse --short origin/main
    
    * git fetch origin main
    
    * main
    
    * deploy on push

--- terraform/main.tf
+++ terraform/main.tf
@@ -46,3 +46,22 @@ resource "google_artifact_registry_repository" "cloud_run" {
     }
   }
 }
+
+resource "google_cloud_run_v2_service" "jravn" {
+  name     = "jravn"
+  location = local.region
+  ingress  = "INGRESS_TRAFFIC_ALL"
+  template {
+    containers {
+      image = "europe-west1-docker.pkg.dev/jensravn/cloud-run/jravn:${var.label}"
+    }
+  }
+}
+resource "google_cloud_run_service_iam_binding" "jravn_iam_binding" {
+  location = google_cloud_run_v2_service.jravn.location
+  service  = google_cloud_run_v2_service.jravn.name
+  role     = "roles/run.invoker"
+  members = [
+    "allUsers"
+  ]
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,6 @@
 variable "project_id" {
   type = string
 }
+variable "label" {
+  type = string
+}
