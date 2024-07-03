913c0a5665538003ecf7d6ac2549f6f8cce4e71c
Jens Ravn Jensen <3356452+jensravn@users.noreply.github.com>
2024-07-03 19:01:43 +0200

    chore: update rand pool (#365)

--- go/go.mod
+++ go/go.mod
@@ -1,6 +1,6 @@
 module github.com/jensravn/jravn
 
-go 1.22.4
+go 1.22.5
 
 require cloud.google.com/go/firestore v1.15.0
 
--- go/internal/question/question.go
+++ go/internal/question/question.go
@@ -5,25 +5,6 @@ import (
 	"time"
 )
 
-var exams = []struct {
-	Exam      string
-	Questions int
-	PageSize  int
-}{
-	{Exam: "associate-cloud-engineer", Questions: 65},
-	{Exam: "cloud-digital-leader", Questions: 70},
-	{Exam: "professional-cloud-architect", Questions: 276},
-	{Exam: "professional-cloud-database-engineer", Questions: 132, PageSize: 4},
-	{Exam: "professional-cloud-developer", Questions: 279},
-	{Exam: "professional-cloud-devops-engineer", Questions: 166, PageSize: 4},
-	{Exam: "professional-cloud-network-engineer", Questions: 172, PageSize: 4},
-	{Exam: "professional-cloud-security-engineer", Questions: 244},
-	{Exam: "professional-collaboration-engineer", Questions: 79},
-	{Exam: "professional-data-engineer", Questions: 311, PageSize: 10},
-	{Exam: "professional-google-workspace-administrator", Questions: 132, PageSize: 4},
-	{Exam: "professional-machine-learning-engineer", Questions: 285},
-}
-
 type Date struct {
 	Date     string `json:"date"`
 	Exam     string `json:"exam"`
@@ -45,6 +26,7 @@ type Comment struct {
 func GetDate(t time.Time) Date {
 	seed := t.UnixNano()
 	r := rand.New(rand.NewPCG(uint64(seed), 2))
+	exams = getExams(t)
 	n := r.IntN(len(exams))
 	e := exams[n]
 	q := r.IntN(e.Questions) + 1
@@ -63,4 +45,47 @@ func getPage(question int, pageSize int) int {
 		pageSize = 5
 	}
 	return (question-1)/pageSize + 1
+}
+
+func getExams(t time.Time) []exam {
+	if t.Before(time.Date(2024, 7, 3, 0, 0, 0, 0, time.UTC)) {
+		return examsUntil2024_07_03
+
+	}
+	return exams
+}
+
+type exam struct {
+	Exam      string
+	Questions int
+	PageSize  int
+}
+
+var exams = []exam{
+	{Exam: "associate-cloud-engineer", Questions: 65},
+	{Exam: "cloud-digital-leader", Questions: 70},
+	{Exam: "professional-cloud-architect", Questions: 276},
+	{Exam: "professional-cloud-database-engineer", Questions: 132, PageSize: 4},
+	{Exam: "professional-cloud-developer", Questions: 279},
+	{Exam: "professional-cloud-devops-engineer", Questions: 166, PageSize: 4},
+	{Exam: "professional-cloud-network-engineer", Questions: 172, PageSize: 4},
+	{Exam: "professional-cloud-security-engineer", Questions: 244},
+	{Exam: "professional-data-engineer", Questions: 311, PageSize: 10},
+	{Exam: "professional-google-workspace-administrator", Questions: 132, PageSize: 4},
+	{Exam: "professional-machine-learning-engineer", Questions: 285},
+}
+
+var examsUntil2024_07_03 = []exam{
+	{Exam: "associate-cloud-engineer", Questions: 65},
+	{Exam: "cloud-digital-leader", Questions: 70},
+	{Exam: "professional-cloud-architect", Questions: 276},
+	{Exam: "professional-cloud-database-engineer", Questions: 132, PageSize: 4},
+	{Exam: "professional-cloud-developer", Questions: 279},
+	{Exam: "professional-cloud-devops-engineer", Questions: 166, PageSize: 4},
+	{Exam: "professional-cloud-network-engineer", Questions: 172, PageSize: 4},
+	{Exam: "professional-cloud-security-engineer", Questions: 244},
+	{Exam: "professional-collaboration-engineer", Questions: 79},
+	{Exam: "professional-data-engineer", Questions: 311, PageSize: 10},
+	{Exam: "professional-google-workspace-administrator", Questions: 132, PageSize: 4},
+	{Exam: "professional-machine-learning-engineer", Questions: 285},
 }
