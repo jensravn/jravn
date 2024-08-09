d66759c34cc91c9690f742493c90b8e202bc66d0
jensravn <jens.ravn.jensen@ingka.com>
2024-08-09 17:04:49 +0200

    feat: style review page

---
+++ typescript/apps/storybook/src/stories/PageQuestionReview.stories.tsx
@@ -0,0 +1,63 @@
+import PageQuestionReview from "@repo/ui/page-question-review";
+import type { Meta, StoryObj } from "@storybook/react";
+import GitHubLogo from "./assets/github-logo.svg";
+
+const meta = {
+  component: PageQuestionReview,
+  parameters: {},
+  args: {
+    gitHubLogo: <img src={GitHubLogo} alt="GitHub" width="24" height="24" />,
+  },
+} satisfies Meta<typeof PageQuestionReview>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    thursdayNote: {
+      data: {
+        comments: [
+          {
+            text: "clever comment that shows reflection and after thought.",
+            timeStamp: "2022-01-01",
+          },
+        ],
+        mostVoted: "A",
+        ourAnswer: "B",
+      },
+      isLoading: false,
+    },
+    thursdayQuestion: {
+      data: {
+        exam: "professional-machine-learning-engineer",
+        page: 25,
+        question: 124,
+        date: "2022-01-01",
+      },
+      isLoading: false,
+    },
+    fridayNote: {
+      data: {
+        comments: [
+          {
+            text: "clever comment that shows reflection and after thought.",
+            timeStamp: "2022-01-01",
+          },
+        ],
+        mostVoted: "B",
+        ourAnswer: "B",
+      },
+      isLoading: false,
+    },
+    fridayQuestion: {
+      data: {
+        exam: "professional-machine-learning-engineer",
+        page: 25,
+        question: 124,
+        date: "2022-01-01",
+      },
+      isLoading: false,
+    },
+  },
+};
