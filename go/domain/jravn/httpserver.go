d766ab65985be82e9fc4b4289d2ccc4c2bdb2baa
Jens Ravn Jensen <3356452+jensravn@users.noreply.github.com>
2025-02-13 22:38:13 +0100

    refactor: routes to named functions (#757)

--- go/domain/jravn/httpserver.go
+++ go/domain/jravn/httpserver.go
@@ -14,44 +14,78 @@ import (
 func NewHTTPServer(projectID string) *http.Server {
 	router := http.NewServeMux()
 
-	// api
-	router.HandleFunc("GET /api/daily-cloud-question/{year}/{month}/{day}", func(w http.ResponseWriter, r *http.Request) {
-		year, err := strconv.Atoi(r.PathValue("year"))
-		if err != nil {
-			http.Error(w, "invalid year", http.StatusBadRequest)
-			return
-		}
-		month, err := strconv.Atoi(r.PathValue("month"))
-		if err != nil {
-			http.Error(w, "invalid month", http.StatusBadRequest)
-			return
-		}
-		day, err := strconv.Atoi(r.PathValue("day"))
-		if err != nil {
-			http.Error(w, "invalid day", http.StatusBadRequest)
-			return
-		}
-		dateString := fmt.Sprintf("%04d-%02d-%02d", year, month, day)
-		t, err := time.Parse(time.DateOnly, dateString)
-		if err != nil {
-			msg := fmt.Sprintf("invalid date: %s", dateString)
-			http.Error(w, msg, http.StatusBadRequest)
-			return
-		}
-		if t.After(time.Now()) {
-			http.Error(w, `cannot get future question`, http.StatusBadRequest)
-			return
-		}
-		q := question.GetDate(t)
-		w.Header().Set("Content-Type", "application/json")
-		err = json.NewEncoder(w).Encode(q)
-		if err != nil {
-			http.Error(w, `internal server error`, http.StatusInternalServerError)
-			return
-		}
-	})
+	// routes - api
+	router.HandleFunc("GET /api/daily-cloud-question/{year}/{month}/{day}", getApiDailyCloudQuestion)
+	router.HandleFunc("GET /api/daily-cloud-question/note/{year}/{month}/{day}", getApiDailyCloudQuestionNote(projectID))
+	router.HandleFunc("POST /api/daily-cloud-question/note/comment/{year}/{month}/{day}", postApiDailyCloudQuestionNoteComment(projectID))
+	router.HandleFunc("PUT /api/daily-cloud-question/note/most-voted/{year}/{month}/{day}", putApiDailyCloudQuestionNoteMostVoted(projectID))
+	router.HandleFunc("PUT /api/daily-cloud-question/note/our-answer/{year}/{month}/{day}", putApiDailyCloudQuestionNoteOurAnswer(projectID))
 
-	router.HandleFunc("GET /api/daily-cloud-question/note/{year}/{month}/{day}", func(w http.ResponseWriter, r *http.Request) {
+	// routes - html
+	router.HandleFunc("GET /404", get404)
+	router.HandleFunc("GET /question", getQuestion)
+	router.HandleFunc("GET /question/review", getQuestionReview)
+
+	// static files
+	fs := http.FileServer(http.Dir("./static"))
+	router.Handle("/", fs)
+
+	return &http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+}
+
+func get404(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "./static/404.html")
+}
+
+func getQuestion(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "./static/question.html")
+}
+
+func getQuestionReview(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "./static/question/review.html")
+}
+
+func getApiDailyCloudQuestion(w http.ResponseWriter, r *http.Request) {
+	year, err := strconv.Atoi(r.PathValue("year"))
+	if err != nil {
+		http.Error(w, "invalid year", http.StatusBadRequest)
+		return
+	}
+	month, err := strconv.Atoi(r.PathValue("month"))
+	if err != nil {
+		http.Error(w, "invalid month", http.StatusBadRequest)
+		return
+	}
+	day, err := strconv.Atoi(r.PathValue("day"))
+	if err != nil {
+		http.Error(w, "invalid day", http.StatusBadRequest)
+		return
+	}
+	dateString := fmt.Sprintf("%04d-%02d-%02d", year, month, day)
+	t, err := time.Parse(time.DateOnly, dateString)
+	if err != nil {
+		msg := fmt.Sprintf("invalid date: %s", dateString)
+		http.Error(w, msg, http.StatusBadRequest)
+		return
+	}
+	if t.After(time.Now()) {
+		http.Error(w, `cannot get future question`, http.StatusBadRequest)
+		return
+	}
+	q := question.GetDate(t)
+	w.Header().Set("Content-Type", "application/json")
+	err = json.NewEncoder(w).Encode(q)
+	if err != nil {
+		http.Error(w, `internal server error`, http.StatusInternalServerError)
+		return
+	}
+}
+
+func getApiDailyCloudQuestionNote(projectID string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
 		year, err := strconv.Atoi(r.PathValue("year"))
 		if err != nil {
 			http.Error(w, "invalid year", http.StatusBadRequest)
@@ -95,9 +129,11 @@ func NewHTTPServer(projectID string) *http.Server {
 			http.Error(w, `internal server error`, http.StatusInternalServerError)
 			return
 		}
-	})
+	}
+}
 
-	router.HandleFunc("PUT /api/daily-cloud-question/note/our-answer/{year}/{month}/{day}", func(w http.ResponseWriter, r *http.Request) {
+func postApiDailyCloudQuestionNoteComment(projectID string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
 		year, err := strconv.Atoi(r.PathValue("year"))
 		if err != nil {
 			http.Error(w, "invalid year", http.StatusBadRequest)
@@ -121,11 +157,7 @@ func NewHTTPServer(projectID string) *http.Server {
 			return
 		}
 		if t.After(time.Now()) {
-			http.Error(w, `cannot save answer for future question`, http.StatusBadRequest)
-			return
-		}
-		if t.Before(time.Now().Add(-24 * time.Hour)) {
-			http.Error(w, `cannot save answer for past question`, http.StatusBadRequest)
+			http.Error(w, `cannot save comment for future question`, http.StatusBadRequest)
 			return
 		}
 		c, err := firestore.NewClient(projectID)
@@ -143,30 +175,27 @@ func NewHTTPServer(projectID string) *http.Server {
 				return
 			}
 		}
-		if n.OurAnswer != "" {
-			http.Error(w, `answer already saved`, http.StatusBadRequest)
-			return
+		type comment struct {
+			Comment string `json:"comment"`
 		}
-		note := question.Note{}
-		err = json.NewDecoder(r.Body).Decode(&note)
+		cmt := comment{}
+		err = json.NewDecoder(r.Body).Decode(&cmt)
 		if err != nil {
 			http.Error(w, `invalid request body`, http.StatusBadRequest)
 			return
 		}
-		if note.OurAnswer != "A" && note.OurAnswer != "B" && note.OurAnswer != "C" && note.OurAnswer != "D" {
-			http.Error(w, `invalid answer`, http.StatusBadRequest)
-			return
-		}
-		n.OurAnswer = note.OurAnswer
+		n.Comments = append(n.Comments, question.Comment{Text: cmt.Comment, TimeStamp: time.Now()})
 		err = repo.Put(t, n)
 		if err != nil {
 			http.Error(w, `internal server error`, http.StatusInternalServerError)
 			return
 		}
 		w.WriteHeader(http.StatusNoContent)
-	})
+	}
+}
 
-	router.HandleFunc("PUT /api/daily-cloud-question/note/most-voted/{year}/{month}/{day}", func(w http.ResponseWriter, r *http.Request) {
+func putApiDailyCloudQuestionNoteMostVoted(projectID string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
 		year, err := strconv.Atoi(r.PathValue("year"))
 		if err != nil {
 			http.Error(w, "invalid year", http.StatusBadRequest)
@@ -233,13 +262,11 @@ func NewHTTPServer(projectID string) *http.Server {
 			return
 		}
 		w.WriteHeader(http.StatusNoContent)
-	})
-
-	type comment struct {
-		Comment string `json:"comment"`
 	}
+}
 
-	router.HandleFunc("POST /api/daily-cloud-question/note/comment/{year}/{month}/{day}", func(w http.ResponseWriter, r *http.Request) {
+func putApiDailyCloudQuestionNoteOurAnswer(projectID string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
 		year, err := strconv.Atoi(r.PathValue("year"))
 		if err != nil {
 			http.Error(w, "invalid year", http.StatusBadRequest)
@@ -263,7 +290,11 @@ func NewHTTPServer(projectID string) *http.Server {
 			return
 		}
 		if t.After(time.Now()) {
-			http.Error(w, `cannot save comment for future question`, http.StatusBadRequest)
+			http.Error(w, `cannot save answer for future question`, http.StatusBadRequest)
+			return
+		}
+		if t.Before(time.Now().Add(-24 * time.Hour)) {
+			http.Error(w, `cannot save answer for past question`, http.StatusBadRequest)
 			return
 		}
 		c, err := firestore.NewClient(projectID)
@@ -281,42 +312,26 @@ func NewHTTPServer(projectID string) *http.Server {
 				return
 			}
 		}
-		comment := comment{}
-		err = json.NewDecoder(r.Body).Decode(&comment)
+		if n.OurAnswer != "" {
+			http.Error(w, `answer already saved`, http.StatusBadRequest)
+			return
+		}
+		note := question.Note{}
+		err = json.NewDecoder(r.Body).Decode(&note)
 		if err != nil {
 			http.Error(w, `invalid request body`, http.StatusBadRequest)
 			return
 		}
-		n.Comments = append(n.Comments, question.Comment{Text: comment.Comment, TimeStamp: time.Now()})
+		if note.OurAnswer != "A" && note.OurAnswer != "B" && note.OurAnswer != "C" && note.OurAnswer != "D" {
+			http.Error(w, `invalid answer`, http.StatusBadRequest)
+			return
+		}
+		n.OurAnswer = note.OurAnswer
 		err = repo.Put(t, n)
 		if err != nil {
 			http.Error(w, `internal server error`, http.StatusInternalServerError)
 			return
 		}
 		w.WriteHeader(http.StatusNoContent)
-	})
-
-	// routes
-	router.HandleFunc("GET /404", get404)
-	router.HandleFunc("GET /question", getQuestion)
-	router.HandleFunc("GET /question/review", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "./static/question/review.html")
-	})
-
-	// static files
-	fs := http.FileServer(http.Dir("./static"))
-	router.Handle("/", fs)
-
-	return &http.Server{
-		Addr:    ":8080",
-		Handler: router,
 	}
-}
-
-func get404(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "./static/404.html")
-}
-
-func getQuestion(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "./static/question.html")
 }
